d75a6ee3e2da1528a40da4955fbde5b997196e57
dkaukov <dkaukov@opentable.com>
2017-09-24 16:48:53 +1000

    HAL - removed SWR calculation.

--- hal100/hal100.ino
+++ hal100/hal100.ino
@@ -100,14 +100,16 @@ void busReceiver(const TCommand *payload, const PJON_Packet_Info &packet_info) {
 		Serial.print(", Value fwd=");
 		Serial.print(payload->status.adc.fwd);
 		Serial.print(", Value rfl=");
-		Serial.print(payload->status.adc.rfl);
+		Serial.println(payload->status.adc.rfl);
 
+		/*
 		float rfl = payload->status.adc.rfl;
 		float fwd = payload->status.adc.fwd;
 		float p = sqrt(rfl / fwd);
 		valueSWR = (1 + p) / (1 - p);
 		Serial.print(", Value SWR=");
 		Serial.println(valueSWR);
+		*/
 		break;
 	}
 }
