ef841942e70a43bf1b16a389b5fd23fcef0fdbee
dkaukov <dkaukov@opentable.com>
2017-11-15 15:38:45 +1100

    Attempt to broadcast status.

--- proto/common.h
+++ proto/common.h
@@ -108,7 +108,7 @@ inline boolean mechCalibrate(uint8_t ch) {
 inline boolean halSendStatusUpdate(PROTO_MechStatus &status) {
   TCommand cmd = { .id = cmdStatus };
   cmd.status = status;
-  return icsc.send(ID_HAL100, cmdStatus, sizeof(cmd.status) + sizeof(cmd.id), (char *) &cmd);
+  return icsc.broadcast(cmdStatus, sizeof(cmd.status) + sizeof(cmd.id), (char *) &cmd);
 }
 
 inline boolean halSendStatusUpdateReply(PROTO_MechStatus &status) {
